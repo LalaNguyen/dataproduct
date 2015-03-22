e37962df2f7635707016a57a472ccd4a73182b59
LalaNguyen <minh.ng0611@gmail.com>
2015-03-22 20:44:15 +0700

    update new chart

--- server.r
+++ server.r
@@ -1,4 +1,7 @@
 library(UsingR)
+require(rCharts)
+options(RCHART_WIDTH = 800)
+
 data(airquality)
 
 shinyServer(
@@ -8,6 +11,23 @@ shinyServer(
       input$goButton
       isolate(paste(input$checkGroup))
       })
+    output$mytable3 <- renderDataTable({
+        
+        airquality     
+     }, options = list(lengthMenu = c(10, 30, 50), pageLength = 10))
+    
+    output$myScatterPlot <- renderChart2({
+         subset <- airquality[airquality$Month==input$selectMonth,]
+         p1 <- rPlot(Wind~Day, data=subset, color="Month",type="point",size = list(const = 3))
+         p1$addParams(width = 600, height = 400, dom = 'chart1',
+                     title = " Wind Scatter Plot on regular daily basis")
+         p1$guides(x = list(title = "", min = 0,max = 32 ))
+         p1$guides(y = list(title = "", max = 22))
+         return(p1)
+         })
+    output$omonth2 <- renderPrint({
+            input$selectMonth
+    })
     output$myLine <- renderPlot({
       input$goButton
       dfm <- airquality[,c("Temp","Day","Month")]
--- ui.R
+++ ui.R
@@ -1,19 +1,52 @@
+require(rCharts)
+options(RCHART_LIB = 'polycharts')
+
 shinyUI(
   pageWithSidebar(
     headerPanel("Temperature Measurement"),
     sidebarPanel(
-      checkboxGroupInput("checkGroup", 
-                              label = h4("Choose month(s)"), 
-                              choices = list("May" = 5, 
+            conditionalPanel(
+                    'input.dataset === "Temp"',
+                    helpText('Visualizing trending in temperature across different months.'),
+                    checkboxGroupInput("checkGroup", 
+                                       label = h5("Available month(s)"), 
+                                       choices = list("May" = 5, 
+                                                      "June" = 6,
+                                                      "July" = 7,
+                                                      "August" = 8,
+                                                      "Septemper" = 9),
+                                       selected = 5),
+                    actionButton("goButton","Go!")
+            ),
+            conditionalPanel(
+                    'input.dataset === "Wind"',
+                    helpText('This chart illustrates Wind Scatter Plot on daily basis.'),
+                    selectInput("selectMonth",label = 'Select Month:', 
+                                choices=list("May" = 5, 
                                              "June" = 6,
                                              "July" = 7,
                                              "August" = 8,
-                                             "Septemper" = 9),
-                              selected = 5),
-      actionButton("goButton","Go!")
+                                            "Septemper" = 9), multiple=TRUE)
+            ),
+            conditionalPanel(
+                    'input.dataset === "Data Source"',
+                    helpText('Display 10 records by default.')
+            )
    ),
     mainPanel(
-      plotOutput("myLine"),
-      verbatimTextOutput("omonth")          
+            tabsetPanel(
+                    id = 'dataset',
+                    tabPanel('Temp',  plotOutput("myLine"),
+                             p("You have selected: "),
+                             verbatimTextOutput("omonth")          ),
+                    tabPanel('Wind', 
+                             showOutput("myScatterPlot","polycharts"),
+                             p("You have selected: "),
+                             verbatimTextOutput("omonth2")   
+                             ),
+                    tabPanel('Data Source', 
+                             dataTableOutput('mytable3'))
+            )
+            
   )
 ))
